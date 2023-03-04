db7b9538e14df039517fbedf6d9fd6ca2d39e7f5
UrazbaevT <utilek990@gmail.com>
2023-03-04 21:22:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static double digit;
+
+    public static void test(int a) {
+        System.out.println(a);
+        //System.out.println(b);
+        int b = 9, age = 23;
+        System.out.println(b);
+        if (8 > 1) {
+            System.out.println(b);
+            int c = 18;
+            System.out.println(c);
+            System.out.println(digit);
+        }
+        //System.out.println(c);
+        System.out.println(digit);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //System.out.println(a);
+        int b = 9;
+        System.out.println(digit);
+
+
+    }
+
+}
